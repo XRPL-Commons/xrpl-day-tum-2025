4d7b6e87b618b7c732fafbd504419509db3af2a2
Mathis Sergent <sergent.mathis@gmail.com>
2025-09-10 12:15:05 +0200

    multisig

--- src/index.ts
+++ src/index.ts
@@ -42,14 +42,107 @@ async function payment() {
     await client.disconnect();
 }
 
+// There is a mistake in my code sorry :( I will fix it soon
 async function multisig() {
+    const client = new Client("wss://s.altnet.rippletest.net:51233");
+
+    await client.connect();
+
+    const { wallet: wallet1 } = await client.fundWallet();
+    const { wallet: wallet2 } = await client.fundWallet();
+    const { wallet: wallet3 } = await client.fundWallet();
+
+    console.log("Wallet 1 address:", wallet1.address);
+    console.log("Wallet 2 address:", wallet2.address);
+    console.log("Wallet 3 address:", wallet3.address);
+
+    // Signer List Setup
+    const signerListSetTx: SignerListSet = {
+        TransactionType: "SignerListSet",
+        Account: wallet1.address,
+        SignerQuorum: 2,
+        SignerEntries: [
+            {
+                SignerEntry: {
+                    Account: wallet1.address,
+                    SignerWeight: 1,
+                },
+            },
+            {
+                SignerEntry: {
+                    Account: wallet2.address,
+                    SignerWeight: 1,
+                },
+            },
+            {
+                SignerEntry: {
+                    Account: wallet3.address,
+                    SignerWeight: 1,
+                },
+            },
+        ],
+    };
+
+    const signerListSetTxResult = await client.submitAndWait(signerListSetTx, { autofill: true, wallet: wallet1 });
+
+    if(signerListSetTxResult.result.validated) {
+        console.log(chalk.green("Signerlist transaction succeeded ✅"));
+    } else {
+        console.log(chalk.red("Signerlist transaction failed ❌ - Result:", signerListSetTxResult.result.meta));
+    }
+
+    // TICKETS
+    const ticketTx: TicketCreate = {
+        TransactionType: "TicketCreate",
+        Account: wallet1.address,
+        TicketCount: 5,
+    };
+
+    const ticketTxResult = await client.submitAndWait(ticketTx, { autofill: true, wallet: wallet1 });
+
+    if(ticketTxResult.result.validated) {
+        console.log(chalk.green("Ticket transaction succeeded ✅"));
+    } else {
+        console.log(chalk.red("Ticket transaction failed ❌ - Result:", signerListSetTxResult.result.meta));
+    }
+
+    // Retrieve
+    const accountTickets = await client.request({
+        command: "account_objects",
+        account: wallet1.address,
+        type: "ticket",
+    });
+
+    console.log("Account tickets:", JSON.stringify(accountTickets.result.account_objects, null, 2));
+
+    // Multisign Payment
+    const multisigPaymentTx: Payment = {
+        TransactionType: "Payment",
+        Account: wallet1.address,
+        Destination: wallet3.address,
+        Amount: xrpToDrops(1)
+    }
+
+    const signedTx1 = wallet1.sign(multisigPaymentTx, true);
+    const signedTx2 = wallet2.sign(multisigPaymentTx, true);
+
+    const multisignedTx = multisign([signedTx1.tx_blob, signedTx2.tx_blob]);
+    
+    const multisigPaymentTxResult = await client.submitAndWait(multisignedTx);
+
+    if(multisigPaymentTxResult.result.validated) {
+        console.log(chalk.green("Multisig payment transaction succeeded ✅"));
+    } else {
+        console.log(chalk.red("Multisig payment transaction failed ❌ - Result:", multisigPaymentTxResult.result.meta));
+    }
+
+    await client.disconnect();
+}
+
+async function batch() {
 
 }
 
 async function amm() {
-
-}
-
-async function batch() {
 
 }
