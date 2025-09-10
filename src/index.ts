326f2a31717fe96886fea6fc8bbc630d15893422
Mathis Sergent <sergent.mathis@gmail.com>
2025-09-10 11:47:49 +0200

    payment

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,43 @@ import { AccountSet, AccountSetAsfFlags, AMMCreate, AMMDeposit, Client, convertS
 import { signMultiBatch, combineBatchSigners } from 'xrpl/dist/npm/Wallet/batchSigner';
 import { Batch, BatchFlags, BatchInnerTransaction } from "xrpl/dist/npm/models/transactions/batch";
 
-
-
 async function payment() {
-    
+    const client = new Client("wss://s.altnet.rippletest.net:51233");
+
+    await client.connect();
+
+    const { wallet: sender, balance } = await client.fundWallet();
+    const { wallet: receiver } = await client.fundWallet();
+
+    console.log("Sender address:", sender.address);
+    console.log("Receiver address:", receiver.address);
+
+    const paymentTx: Payment = {
+        TransactionType: "Payment",
+        Account: sender.address,
+        Amount: xrpToDrops(1),
+        Destination: receiver.address,
+        Memos: [
+            {
+                Memo: {
+                    MemoType: convertStringToHex("Message"),
+                    MemoData: convertStringToHex("Hello, TUM!"),
+                },
+            },
+        ],
+    }
+
+    const paymentTxResult = await client.submitAndWait(paymentTx, { autofill: true, wallet: sender });
+
+    console.log("Payment transaction hash:", paymentTxResult.result.hash);
+
+    if(paymentTxResult.result.validated) {
+        console.log(chalk.green("Payment transaction succeeded ✅"));
+    } else {
+        console.log(chalk.red("Payment transaction failed ❌ - Result:", paymentTxResult.result.meta));
+    }
+
+    await client.disconnect();
 }
 
 async function multisig() {
@@ -14,9 +47,9 @@ async function multisig() {
 }
 
 async function amm() {
-    
+
 }
 
 async function batch() {
-    
+
 }
